4ff4149bcdb9a61335f696c119ed21f65005e8d0
[200~1017913943@qq.com <1017913943@qq.com>
2024-03-13 22:30:25 +0800

    init react

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+    console.log("s")
+    return (
+        <div>
+            <h1>Welcome to React Web.</h1>
+        </div>
+    );
+}
